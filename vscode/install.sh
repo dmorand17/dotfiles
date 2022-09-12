b847bd8a466f30489a6bb54efcaf9d868d8f4d41
Doug Morand <dmorand@gmail.com>
2022-09-12 10:18:24 -0700

    Update install.sh

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,15 +2,16 @@
 
 set -e
 
-os="$(uname -s)"
-case "${os}" in 
-    Linux*)
-        ln -sf "${DOTFILES_LOCATION}/vscode/settings.json" "${HOME}/.config/Code/User/settings.json"
-        ;;
-    Darwin*)
-        ln -sf "${DOTFILES_LOCATION}/vscode/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
-        # ln -sf "${DOTFILES_LOCATION}/vscode/keybindings.json" "${HOME}/Library/Application Support/Code/User/keybindings.json"
-        ;;
-    *) ;;
-esac
-
+if [ -x "$(command -v code)" ]; then
+    os="$(uname -s)"
+    case "${os}" in 
+        Linux*)
+            ln -sf "${DOTFILES_LOCATION}/vscode/settings.json" "${HOME}/.config/Code/User/settings.json"
+            ;;
+        Darwin*)
+            ln -sf "${DOTFILES_LOCATION}/vscode/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
+            # ln -sf "${DOTFILES_LOCATION}/vscode/keybindings.json" "${HOME}/Library/Application Support/Code/User/keybindings.json"
+            ;;
+        *) ;;
+    esac
+fi
