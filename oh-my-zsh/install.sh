1b9e77f9f98657332a36a9054b5808d1f2492812
Doug Morand <dmorand@gmail.com>
2022-12-02 14:49:27 +0000

    Fixing up zsh

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -14,7 +14,4 @@ else
   curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 fi
 
-ln -sf "${DOTFILES_LOCATION}/oh-my-zsh/.aliases" "${HOME}/.aliases"
-ln -sf "${DOTFILES_LOCATION}/oh-my-zsh/.functions" "${HOME}/.functions"
-ln -sf "${DOTFILES_LOCATION}/oh-my-zsh/.exports" "${HOME}/.exports"
 ln -sf "${DOTFILES_LOCATION}/oh-my-zsh/zplug.zsh" "${HOME}/.zplug.zsh"
--- zsh/install.sh
+++ zsh/install.sh
@@ -7,3 +7,8 @@ ln -sf "${DOTFILES_LOCATION}/zsh/.zshrc" "${HOME}/.zshrc"
 if [[ ! ${REMOTE_CONTAINERS} ]] ; then
   sudo chsh -s $(which zsh)
 fi
+
+
+ln -sf "${DOTFILES_LOCATION}/.aliases" "${HOME}/.aliases"
+ln -sf "${DOTFILES_LOCATION}/.functions" "${HOME}/.functions"
+ln -sf "${DOTFILES_LOCATION}/.exports" "${HOME}/.exports"
