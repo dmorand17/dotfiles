ca1305e447226775e9858a56b9e90a3e161715f8
Doug Morand <dmorand@gmail.com>
2022-12-02 07:56:49 -0500

    Update install.sh

--- ssh/install.sh
+++ ssh/install.sh
@@ -7,3 +7,4 @@ ln -sf "${DOTFILES_LOCATION}/ssh/.ssh-agent" "${HOME}/.ssh-agent"
 if [[ ! -d "${HOME}/.ssh" ]]; then mkdir "${HOME}/.ssh"; fi
 ln -sf "${DOTFILES_LOCATION}/ssh/config" "${HOME}/.ssh/config"
 ln -sf "${DOTFILES_LOCATION}/ssh/add-keys" "${HOME}/.ssh/add-keys"
+touch ${HOME}/.ssh/config-local
